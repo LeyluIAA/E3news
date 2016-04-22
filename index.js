15377647fa1ced22f066c1da50684008772afe5f
LeyluIAA <leyluiaa@outlook.com>
2016-04-22 17:57:41 +0200

    expressjs and feed-read

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const http = require('http');
+var feed = require('feed-read');
+var express = require('express');
+var app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/articles', function (req, res) {
+	  feed('http://www.jeuxvideo.com/rss/rss.xml', function(err, articles) {
+		  if (err) throw err;
+		  res.send('coucoucoucoucou');
+		  //response.push({title: articles.title, date: articles.published});
+		  // Each article has the following properties:
+		  // 
+		  //   * "title"     - The article title (String).
+		  //   * "author"    - The author's name (String).
+		  //   * "link"      - The original article link (String).
+		  //   * "content"   - The HTML content of the article (String).
+		  //   * "published" - The date that the article was published (Date).
+		  //   * "feed"      - {name, source, link}
+  	  });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
