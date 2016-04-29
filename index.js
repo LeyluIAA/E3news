fa950fb55ad63bcf46a17c4948450805f48f2f4e
florent <florent@box.(none)>
2016-04-29 18:01:54 +0200

    multisource ok, has to by sorted by date

--- index.js
+++ index.js
@@ -9,26 +9,36 @@ app.set('views', './views');
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-	var tab = [];
-	feed('http://www.jeuxvideo.com/rss/rss.xml', function(err, articles) {
-		  if (err) throw err;
-		  var articles_length = articles.length;
-		  for (var i = 0; i < articles_length; i++) {
-		  	//var publication = moment(articles[i].published).format("dd/mm/yyyy HH:MM:ss").substract(1, 'hours');
+	
+    var rss = ['http://www.jeuxvideo.com/rss/rss.xml', 'http://www.gameblog.fr/rss.php'],
+        tab = [];
+
+    feed(rss, function(err, articles) {
+		  
+          if (err) throw err;
+		  
+          var articles_length = articles.length;
+		  
+          for (var i = 0; i < articles_length; i++) {
+
             var publication = moment(articles[i].published).subtract(1, 'hours');
 		  	articles[i].published = publication.format("DD/MM/YYYY HH:mm:ss");
-            console.log(publication);
+
 		  }
-		  res.render('index', {articles: articles});
-		  // Each article has the following properties:
-		  //   * "title"     - The article title (String).
-		  //   * "author"    - The author's name (String).
-		  //   * "link"      - The original article link (String).
-		  //   * "content"   - The HTML content of the article (String).
-		  //   * "published" - The date that the article was published (Date).
-		  //   * "feed"      - {name, source, link}
-  	});
+          console.log('articles', articles);
+          tab = articles;
+          res.render('index', {articles: tab});
+          // Each article has the following properties:
+          //   * "title"     - The article title (String).
+          //   * "author"    - The author's name (String).
+          //   * "link"      - The original article link (String).
+          //   * "content"   - The HTML content of the article (String).
+          //   * "published" - The date that the article was published (Date).
+          //   * "feed"      - {name, source, link}
+    });
 });
+
+
 
 app.listen(3000, function () {
   console.log('E3 News listening on port 3000!');
