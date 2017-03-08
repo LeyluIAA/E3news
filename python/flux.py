82b4acda852a03b58296058d1dea1d57ed696240
Vagrant Default User <vagrant@contrib-jessie.raw>
2017-03-08 16:58:54 +0000

    flux works for several sources

--- python/flux.py
+++ python/flux.py
@@ -2,24 +2,37 @@
 
 import feedparser
 
-# urls = ["http://www.jeuxvideo.com/rss/rss.xml","http://www.gameblog.fr/rss.php","http://www.gamekult.com/feeds/actu.html","http://fr.ign.com/feed.xml","https://www.indiemag.fr/feed/rss.xml","http://www.gaminfo.fr/podcasts/feed"]
+urls = ["http://www.jeuxvideo.com/rss/rss.xml","http://www.gameblog.fr/rss.php","http://www.gamekult.com/feeds/actu.html","http://fr.ign.com/feed.xml","https://www.indiemag.fr/feed/rss.xml","http://www.gaminfo.fr/podcasts/feed"]
 
-url = "http://www.jeuxvideo.com/rss/rss.xml"
-response = feedparser.parse(url)
+for url in urls:
 
-for entry in response.entries:
-    title = entry.title.encode('utf8')
-    description = entry.description.encode('utf-8')
-    link = entry.link.encode('utf8')
-    id = entry.id.encode('utf8')
-    published = entry.published.encode('utf8')
-    tags = []
-    for tag in entry.tags:
-        tags.append(tag.term)
+    response = feedparser.parse(url)
 
-site = response.feed.title.encode('utf8')
-link_site = response.feed.link.encode('utf8')
-description_site = response.feed.description.encode('utf8')
-subtitle = response.feed.subtitle.encode('utf8')
-author_site = response.feed.author.encode('utf8')
-category = response.feed.category.encode('utf8')
+    for entry in response.entries:
+        title = entry.title.encode('utf8')
+        description = entry.description.encode('utf-8')
+        link = entry.link.encode('utf8')
+        id = entry.id.encode('utf8')
+        published = entry.published.encode('utf8')
+        try:
+            tags = []
+            for tag in entry.tags:
+                tags.append(tag.term)
+        except Exception:
+            print('no tags...')
+
+    site = response.feed.title.encode('utf8')
+    link_site = response.feed.link.encode('utf8')
+    description_site = response.feed.description.encode('utf8')
+    subtitle = response.feed.subtitle.encode('utf8')
+    try:
+        author_site = response.feed.author.encode('utf8')
+    except Exception:
+        print('error in author attribute')
+    try:
+        category = response.feed.category.encode('utf8')
+    except Exception:
+        print('error in category attribute')
+    
+    # here: store information in Mongo
+
