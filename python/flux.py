b569f35fb8f05c907682e75f9902d7f32746fd90
LeyluIAA <leyluiaa@outlook.com>
2017-03-09 16:54:05 +0100

    Update flux.py

--- python/flux.py
+++ python/flux.py
@@ -11,29 +11,7 @@ for url in urls:
 
     response = feedparser.parse(url)
     
-    articles = []
-
-    for entry in response.entries:
-        title = entry.title.encode('utf8')
-        description = entry.description.encode('utf-8')
-        link = entry.link.encode('utf8')
-        id = entry.id.encode('utf8')
-        published = entry.published.encode('utf8')
-        tags = []
-        try:
-            for tag in entry.tags:
-                tags.append(tag.term.encode('utf8'))
-        except Exception:
-            pass
-        articles.append({
-            'title': title,
-            'description': description,
-            'link': link,
-            'id': id,
-            'published': published,
-            'tags': tags
-        })
-
+    # Site part
     site = response.feed.title.encode('utf8')
     link_site = response.feed.link.encode('utf8')
     description_site = response.feed.description.encode('utf8')
@@ -47,14 +25,34 @@ for url in urls:
     except Exception:
         print('error in category attribute')
     
-    # here: store information in Mongo
-    post = {
-        'site': site,
-        'link_site': link_site,
-        'description_site': description_site,
-        'subtitle': subtitle,
-        'author_site': author_site,
-        'category': category,
-        'articles': articles
-    }
-    print(post)
+    # Posts part
+    for entry in response.entries:
+        title = entry.title.encode('utf8')
+        description = entry.description.encode('utf-8')
+        link = entry.link.encode('utf8')
+        id = entry.id.encode('utf8')
+        published = entry.published.encode('utf8')
+        tags = []
+        try:
+            for tag in entry.tags:
+                tags.append(tag.term.encode('utf8'))
+        except Exception:
+            pass
+        
+        # Article construction
+        article = {
+            'site': site,
+            'link_site': link_site,
+            'description_site': description_site,
+            'subtitle': subtitle,
+            'author_site': author_site,
+            'category': category
+            'title': title,
+            'description': description,
+            'link': link,
+            'id': id,
+            'published': published,
+            'tags': tags
+        })
+    
+        # Store information in Mongo
