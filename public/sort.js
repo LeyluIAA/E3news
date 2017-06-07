7a60c16df2147e714d5996bea0d7175e161bf6d2
fdemeulenaere <fdemeulenaere@capensis.fr>
2017-06-07 17:43:02 +0200

    better UX

--- public/sort.js
+++ public/sort.js
@@ -1,3 +1,16 @@
+$(document).ready(function(){
+    window.setInterval(function(){refresh()}, 120000);
+});
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
 $("#search").keyup(function() {
     var value = $(this).val();
     value = value.toLowerCase();
@@ -23,6 +36,96 @@ $('.expand').click(function(){
     $('.expand-content').slideToggle('slow');
 });
 
-$('.expand-video').click(function(){
-    $('.expanded-video').slideToggle('slow');
+var refresh = function(){
+    window.location.reload(true);
+}
+
+$('#xbox').click(function(){
+    if (!window.xboxfilter){
+        $("table tr").each(function(index) {
+            if (index !== 0) {
+
+                $row = $(this);
+
+                var id = $row.find("td:first").text();
+                id = id.toLowerCase();
+                if (id.match('xbox') === null && id.match('microsoft') === null) {
+                    $row.hide();
+                }
+                else {
+                    $row.show();
+                }
+            }
+        });
+        window.xboxfilter = true;
+    } else {
+        reset();
+        window.xboxfilter = false;
+    }
 });
+
+$('#playstation').click(function(){
+    if (!window.playstationfilter){
+        $("table tr").each(function(index) {
+            if (index !== 0) {
+
+                $row = $(this);
+
+                var id = $row.find("td:first").text();
+                id = id.toLowerCase();
+                if (id.match('playstation') === null && id.match('sony') === null && id.match('ps4') === null) {
+                    $row.hide();
+                }
+                else {
+                    $row.show();
+                }
+            }
+        });
+        window.playstationfilter = true;
+    } else {
+        reset();
+        window.playstationfilter = false;
+    }
+});
+
+$('#nintendo').click(function(){
+    if (!window.nintendofilter){
+        $("table tr").each(function(index) {
+            if (index !== 0) {
+
+                $row = $(this);
+
+                var id = $row.find("td:first").text();
+                id = id.toLowerCase();
+                if (id.match('nintendo') === null && id.match('switch') === null && id.match('3ds') === null && id.match('2ds') === null) {
+                    $row.hide();
+                }
+                else {
+                    $row.show();
+                }
+            }
+        });
+        window.nintendofilter = true;
+    } else {
+        reset();
+        window.nintendofilter = false;
+    }
+});
+
+var reset = function(){
+    $("table tr").each(function(index) {
+        if (index !== 0) {
+
+            $row = $(this);
+
+            var id = $row.find("td:first").text();
+            id = id.toLowerCase();
+            if (id.match('') === null) {
+                $row.hide();
+            }
+            else {
+                $row.show();
+            }
+        }
+    });
+};
