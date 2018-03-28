b972bee80d0c867ce2955becdaf75444a215743b
leyluiaa <leyluiaa@localhost.localdomain>
2018-03-28 17:52:00 +0200

    integration of bulma: ok with link but no customisation possible

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,71 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <h1 class="title">
-        Hello World
-      </h1>
-      <p class="subtitle">
-        My first website with <strong>Bulma</strong>!
-      </p>
+  <section>
+    <nav class="navbar is-fixed-top is-dark">
+  <div class="navbar-brand">
+    <!--<img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">-->
+    <a class="navbar-item" href="#">E3 News</a>
+    <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
+  </div>
+
+  <div id="navbarExampleTransparentExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="#">
+        Feeds
+      </a>
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link" href="#">
+          Articles
+        </a>
+        <div class="navbar-dropdown is-boxed">
+            <a class="navbar-item" href="#">
+                Tous les articles
+            </a>
+            <hr class="navbar-divider">            
+            <a class="navbar-item" href="#">
+                Microsoft
+            </a>
+            <a class="navbar-item" href="#">
+                Sony
+            </a>
+            <a class="navbar-item" href="#">
+                Nintendo
+            </a>
+            <a class="navbar-item" href="#">
+                PC
+            </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="field is-grouped">
+            <p class="control">
+            <a class="button is-primary" href="#">
+              <span class="icon">
+                <i class="fas fa-user"></i>
+              </span>
+              <span>S'inscrire</span>
+            </a>
+            </p>
+            <p class="control">
+            <a class="button is-link" href="#">
+              <span class="icon">
+                <i class="fas fa-user"></i>
+              </span>
+              <span>Se connecter</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
   </section>
 </template>
 
